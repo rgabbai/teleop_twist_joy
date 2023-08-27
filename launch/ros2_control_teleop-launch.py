673532873385446864e3fff59867541cb6c09e16
rgabbai <rony.gabbai@gmail.com>
2023-08-27 11:23:29 +0300

    base code

---
+++ launch/ros2_control_teleop-launch.py
@@ -0,0 +1,46 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+import launch
+import launch_ros.actions
+
+
+def generate_launch_description():
+    joy_config = launch.substitutions.LaunchConfiguration('joy_config')
+    joy_dev = launch.substitutions.LaunchConfiguration('joy_dev')
+    config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
+
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument('joy_vel', default_value='cmd_vel'),
+        launch.actions.DeclareLaunchArgument('joy_config', default_value='logitech'),
+        launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
+        launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
+            launch.substitutions.TextSubstitution(text=os.path.join(
+                get_package_share_directory('teleop_twist_joy'), 'config', '')),
+            joy_config, launch.substitutions.TextSubstitution(text='.config.yaml')]),
+
+        launch_ros.actions.Node(
+            package='joy', executable='joy_node', name='joy_node',
+            parameters=[{
+                'dev': joy_dev,
+                'deadzone': 0.1,
+                'autorepeat_rate': 10.0,
+                #'default_trig_val': True,
+            }]),
+        launch_ros.actions.Node(
+            package='teleop_twist_joy', executable='teleop_node',
+            name='teleop_twist_joy_node', parameters=[config_filepath],
+            #remappings={('/cmd_vel', '/diff_cont/cmd_vel_unstamped')},
+            remappings={('/cmd_vel', '/cmd_vel_joy')},
+            ),
+        launch_ros.actions.Node(
+            package='teleop_twist_joy', executable='teleop_node',
+            name='teleop_twist_joy_node', parameters=[config_filepath],
+            ),    
+        # For Gazebo sim 
+        #launch_ros.actions.Node(
+        #    package='teleop_twist_joy', executable='teleop_node',
+        #    name='teleop_twist_joy_node', parameters=[config_filepath],
+        #    ),
+    ])
